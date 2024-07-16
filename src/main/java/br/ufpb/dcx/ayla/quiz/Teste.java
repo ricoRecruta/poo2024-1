0b19f7c2de24046330addf1e24adce08bfa607d4
ricoRecruta <miguel.santos@dcx.ufpb.br>
2024-07-16 09:36:32 -0300

    adcionando arquivos do Exercicio 3

---
+++ src/main/java/br/ufpb/dcx/ayla/quiz/Teste.java
@@ -0,0 +1,10 @@
+package br.ufpb.dcx.ayla.quiz;
+
+public class Teste {
+    public static void main(String[] args) {
+        Pergunta pergunta = new PerguntaDissertativa();
+        System.out.println(pergunta.toString());
+
+    }
+
+}
